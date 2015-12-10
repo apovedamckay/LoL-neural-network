c78e8e5ab1796d54ee2861c3464e115ee183a0d0
Adrian Poveda <apovedamckay@umass.edu>
2015-12-09 20:26:43 -0500

    base neural network fixed

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,6 +4,7 @@ from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer
 from pybrain.structure.modules   import SigmoidLayer
+from pybrain.structure.modules   import TanhLayer
 from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
 from scipy import diag, arange, meshgrid, where
 from numpy.random import multivariate_normal
@@ -15,11 +16,31 @@ DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
 DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
 DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
 DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
-DS.appendLinked([ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], [0]);
-DS.appendLinked([ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], [0]);
-DS.appendLinked([ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], [0]);
-DS.appendLinked([ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], [0]);
-DS.appendLinked([ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 ], [0]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ], [0]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
+DS.appendLinked([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ], [1]);
 
 # means = [(-1,0),(2,4),(3,1)]
 # cov = [diag([1,1]), diag([0.5,1.2]), diag([1.5,0.7])]
@@ -41,7 +62,7 @@ print "First sample (input, target, class):"
 print trndata['input'][0], trndata['target'][0]
 # print trndata['input'][0], trndata['target'][0], trndata['class'][0]
 
-fnn = buildNetwork( trndata.indim, 9, trndata.outdim, outclass=SigmoidLayer, hiddenclass=SigmoidLayer )
+fnn = buildNetwork( trndata.indim, 9, trndata.outdim, outclass=TanhLayer, hiddenclass=TanhLayer )
 
 trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
 
@@ -54,7 +75,7 @@ trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, wei
 # griddata._convertToOneOfMany()  # this is still needed to make the fnn feel comfy
 
 for i in range(20):
-    trainer.trainEpochs( 5 )
+    trainer.trainEpochs(50)
     # trainer.trainUntilConvergence();
     trnresult = percentError( trainer.testOnClassData(),
                               trndata['class'] )
@@ -78,6 +99,11 @@ for i in range(20):
 #         contourf(X, Y, out)   # plot the contour
 #     ion()   # interactive graphics on
 #     draw()  # update the plot
-print fnn.activate([ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ])
+print "activated with 1: "
+print fnn.activate([ 1, 1, 1, 1, 1, 1, 1, 1, 1 ]);
+print "activated with 0: "
+print fnn.activate([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]);
+print "activated with -1: "
+print fnn.activate([-1, -1, -1, -1, -1, -1, -1, -1, ]);
 # ioff()
 # show()
